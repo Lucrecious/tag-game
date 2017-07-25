c647884873bc18189414428147752c0001193e8f
Lucrecious <luca.duran@hotmail.com>
2017-07-25 09:56:48 -0700

    Add basic files

---
+++ core/src/taggame/inst/iScene.java
@@ -0,0 +1,17 @@
+package taggame.inst;
+
+import com.badlogic.gdx.utils.JsonValue;
+import com.nilunder.bdx.Instantiator;import com.nilunder.bdx.GameObject;
+import taggame.*;
+public class iScene extends Instantiator {
+
+	public GameObject newObject(JsonValue gobj){
+		String name = gobj.name;
+
+		if (gobj.get("class").asString().equals("Sacky"))
+			return new taggame.Sacky();
+
+		return super.newObject(gobj);
+	}
+	
+}
---
+++ desktop/src/taggame/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package taggame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import taggame.BdxApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "gear";
+		config.width = 800;
+		config.height = 450;
+		new LwjglApplication(new BdxApp(), config);
+	}
+}
