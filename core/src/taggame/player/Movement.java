faade3d9d20901ccb59ddb317e20ac953a4a7e1a
Lucrecious <luca.duran@hotmail.com>
2017-07-25 11:33:25 -0700

    Fix errors

--- core/src/taggame/player/Movement.java
+++ core/src/taggame/player/Movement.java
@@ -11,7 +11,6 @@ public class Movement extends Component {
         State core = new State() {
             @Override
             public void main() {
-                g.collisionGroup();
             }
         };
 
